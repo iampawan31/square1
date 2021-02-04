6ea8142cce550ab10bb9e33c7d22ce4570e5617c
Pawan Kumar <surpawan@gmail.com>
2021-02-04 19:30:45 +0530

    Initial Commit

---
+++ database/migrations/2021_02_04_135527_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->text('title');
+            $table->text('slug')->nullable();
+            $table->longText('description');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamp('publication_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
